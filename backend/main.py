957ec6875bae142506de4f1ca10b25198b3d0f44
Bhaskar C <bhaskarchandru19@gmail.com>
2026-06-09 12:54:03 +0530

    Temporary CORS debug

--- backend/main.py
+++ backend/main.py
@@ -11,15 +11,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "https://rean-pi.vercel.app/"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 UPLOAD_FOLDER = "uploads"
 
